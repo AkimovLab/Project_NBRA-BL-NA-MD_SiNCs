380d36f76881b454bedaee33d0e37a2d1fadde25
Brendan A. Smith <bsmith24@buffalo.edu>
2020-01-17 11:13:40 -0500

    Added copyright information to thermal.py and add_vector.py

--- normal_modes/F19/0p8nm/add_vector.py
+++ normal_modes/F19/0p8nm/add_vector.py
@@ -1,3 +1,12 @@
+#***********************************************************
+# * Copyright (C) 2020 Brendan A. Smith and Alexey V. Akimov
+# * This file is distributed under the terms of the
+# * GNU General Public License as published by the
+# * Free Software Foundation; either version 3 of the
+# * License, or (at your option) any later version.
+# * http://www.gnu.org/copyleft/gpl.txt
+#***********************************************************/
+
 import sys
 import os
 
--- step1/thermal.py
+++ step1/thermal.py
@@ -1,3 +1,12 @@
+#***********************************************************
+# * Copyright (C) 2020 Brendan A. Smith and Alexey V. Akimov
+# * This file is distributed under the terms of the
+# * GNU General Public License as published by the
+# * Free Software Foundation; either version 3 of the
+# * License, or (at your option) any later version.
+# * http://www.gnu.org/copyleft/gpl.txt
+#***********************************************************/
+
 from liblibra_core import *
 from libra_py import *
 import sys
